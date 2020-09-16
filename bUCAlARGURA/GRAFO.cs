ee08c9ee66951eedeea4a3d8337c17aae4003a61
Yurinabaia <yurinabaiaduarte@gmail.com>
2020-09-16 17:11:45 -0300

    BUSCA DE LARGURA, GRAFOS

---
+++ bUCAlARGURA/GRAFO.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bUCAlARGURA
+{
+    enum LETRAS
+    {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G,
+        H
+    }
+    class GRAFO
+    {
+        private int INFINITO;
+        private int V;
+        private int A;
+        private bool[,] adj;
+
+    public GRAFO(int V)
+        {
+            this.V = V;
+            this.A = 0;
+            this.adj = new bool[V,V];
+            this.INFINITO = V;
+        }
+
+        public void adicionaAresta(int u, int v)
+        {
+            if (!adj[u,v])
+            {
+                A++;
+                adj[u,v] = adj[v,u] = true;
+            }
+        }
+
+        public void removeAresta(int u, int v)
+        {
+            if (adj[u,v])
+            {
+                A--;
+                adj[u,v] = adj[v,u] = false;
+            }
+        }
+
+        public int TMGCaminhoMaisCurto(int s, int t)
+        {
+            int[] dist = new int[V];
+            for (int w = 0; w < V; w++)
+            {
+                dist[w] = INFINITO;
+            }
+            buscaEmLargura(s, dist);
+            return dist[t];
+        }
+
+        private void buscaEmLargura(int s, int[] dist)
+        {
+            Queue<int> fila = new Queue<int>();
+            fila.Enqueue(s);
+            LinkedList<int> my_list = new LinkedList<int>();
+           my_list.AddLast(s);
+            dist[s] = 0;
+
+            while (fila.Count > 0)
+            {
+                int u = fila.Dequeue();
+               my_list.RemoveLast();
+                for (int v = 0; v < V; v++)
+                {
+                    if (adj[u,v] && dist[v] == INFINITO)
+                    {
+                        fila.Enqueue(v);
+                      my_list.AddLast(v);
+                        dist[v] = dist[u] + 1;
+                    }
+                }
+            }
+        }
+
+        public List<int> CaminhoMenor(int s, int t)
+        {
+            int[] ant = new int[V];
+            for (int j = 0; j < V; j++)
+            {
+                ant[j] = -1;
+            }
+            ant[s] = s;
+            buscaEmLarguras(s, ant);
+
+            if (ant[t] == -1) return new List<int>();
+
+            List<int> caminho = new List<int>();
+            int w = t;
+            while (w != ant[w])
+            {
+                caminho.Add(w);
+                w = ant[w];
+            }
+            caminho.Add(s); 
+            caminho.Reverse();
+            return caminho;
+        }
+
+        private void buscaEmLarguras(int s, int[] ant)
+        {
+            Queue<int> fila = new Queue<int>();
+            LinkedList<int> my_list = new LinkedList<int>();
+            my_list.AddLast(s);
+            fila.Enqueue(s);
+            ant[s] = s;
+
+            while (fila.Count > 0)
+            {
+                int u = fila.Dequeue();
+                my_list.RemoveLast();
+
+                for (int v = 0; v < V; v++)
+                {
+                    if (adj[u,v] && ant[v] == -1)
+                    {
+                        fila.Enqueue(v);
+                        my_list.AddLast(v);
+                        ant[v] = u;
+                    }
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("Numero de vértices: " + V + "\n");
+            stringBuilder.Append("Numero de arestas: " + A + "\n");
+            int cont = 0;
+            for (int u = 0; u < V; u++)
+            {
+                stringBuilder.Append(LETRA(u) + ":");
+                for (int v = 0; v < V; v++)
+                {
+                    if (adj[u, v])
+                    {
+                        string a = LETRA(v);
+                        stringBuilder.Append(" " + a);
+                        cont++;
+                    }
+                }
+                stringBuilder.Append('\n');
+            }
+            return stringBuilder.ToString();
+        }
+
+        public string LETRA(int pos) 
+        {
+            string palavra = "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
+            return palavra.ToCharArray()[pos].ToString();
+        }
+    }
+}
---
+++ bUCAlARGURA/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace bUCAlARGURA
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Primeiro definir a quantidade de vertice vai possuir o grafo
+            //COLOQUE O VALOR NO GRAFO(8)
+            GRAFO G = new GRAFO(8);
+            //ABAIXO TEMO O GRAFO E SUAS LIGAÇÕES
+            //DEFINIR AS LIGAÇÕES DE ACORDO DO MENOR PARA O MAIOR
+            //DE 0 ATE 1, DE 0 ATE 4, DE 1 ATE 5 ETC
+
+            //***** JEITO CERTO  G.adicionaAresta(2, 5);  **********//
+            //***** NÃO FACA ASSIM 5 ATE 2 VAI DAR MERDA.  G.adicionaAresta(5, 2); error **********//
+
+
+            //0 == A
+            //1 == B
+            //2 == C
+            //3 == D
+            //4 == E
+            //ETC
+
+            G.adicionaAresta(0, 1);
+            G.adicionaAresta(0, 4);
+            G.adicionaAresta(1, 5);
+            G.adicionaAresta(2, 5);
+            G.adicionaAresta(2, 6);
+            G.adicionaAresta(2, 3);
+            G.adicionaAresta(3, 6);
+            G.adicionaAresta(3, 7);
+            G.adicionaAresta(5, 6);
+            G.adicionaAresta(6, 7);
+
+
+            Console.WriteLine(G.TMGCaminhoMaisCurto(7, 0));
+            Console.WriteLine("Menor caminho");
+            foreach (int item in G.CaminhoMenor(7,0))
+            {
+               string asd  = LETRA(item);
+                Console.Write(asd + " - ");
+            }
+            Console.WriteLine("\n");
+
+            Console.WriteLine(G.ToString());
+        }
+        public static string LETRA(int pos)
+        {
+            string palavra = "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
+            return palavra.ToCharArray()[pos].ToString();
+        }
+    }
+}
+
+
